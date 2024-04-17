f8f7ef5160f6d0b330103d62eae640febba56d48
anaramanli <riadhamidov@gmail.com>
2024-04-17 18:21:20 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int[] nums = { 5, 1, 9, 6, 4, 2 };
+
+int max = nums[0];
+int min = nums[0];
+
+for (int i = 1; i < nums.Length; i++)
+{
+    if (nums[i] <= min)
+    {
+        min = nums[i];
+    }
+    else if (nums[i]>=max)
+    {
+        max = nums[i];
+    }
+}
+
+Console.WriteLine($"{min},{max}");
+
